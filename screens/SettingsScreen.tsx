459d48de310005c0f7c13162c101f343bea5745f
Emanuel Turis <emanuelturis@gmail.com>
2020-10-25 17:30:07 +0000

    Add navigation with @react-navigation/native and @react-navigation/bottom-tabs

---
+++ screens/SettingsScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Container} from '../styles';
+
+const SettingsScreen = () => {
+  return (
+    <Container>
+      <Text>Settings Screen</Text>
+    </Container>
+  );
+};
+
+export default SettingsScreen;
