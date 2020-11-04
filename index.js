5f8ccd80a42811bd75bc6595739dd114eab4d7be
Emanuel Turis <emanuelturis@gmail.com>
2020-11-04 21:23:26 +0000

    Rearrange folders by moving everything into a src folder

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 import 'react-native-get-random-values';
 
 import {AppRegistry, Platform} from 'react-native';
-import App from './App';
+import App from './src/components/App';
 import {name as appName} from './app.json';
 import PushNotification from 'react-native-push-notification';
 
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,12 @@ import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import TimerScreen from './screens/TimerScreen';
-import SettingsScreen from './screens/SettingsScreen';
+import TimerScreen from '../screens/TimerScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
-import TimerContextProvider from './contexts/timerContext';
-import {createTable} from './utils/db/createTable';
-import StatsScreen from './screens/StatsScreen';
+import TimerContextProvider from '../contexts/timerContext';
+import {createTable} from '../utils/db/createTable';
+import StatsScreen from '../screens/StatsScreen';
 
 const Tab = createBottomTabNavigator();
 
